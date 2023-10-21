74aab872f8b8d87e3b10903368f39da87754c419
islam solaiman <islam.solaiman92@gmail.com>
2023-10-21 15:58:54 -0700

    Web framework - Task 3 update

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -28,39 +28,6 @@ def c(text):
     return 'C {}'.format(text.replace("_", " "))
 
 
-if __name__ == "__main__":
-    app.run(host='0.0.0.0', port='5000'#!/usr/bin/python3
-"""
-    script that starts a Flask web application
-    web application must be listening on 0.0.0.0, port 5000
-    Routes: /: display “Hello HBNB!”
-    /hbnb: display “HBNB”
-    /c/<text>: display “C ” followed by the value of the text variable
-    (replace underscore _ symbols with a space )
-        The default value of text is “is cool”
-"""
-
-from flask import Flask
-
-app = Flask(__name__)
-
-
-@app.route('/', strict_slashes=False)
-def hello():
-    return "Hello HBNB!"
-
-
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    return "HBNB"
-
-
-@app.route("/c/<text>", strict_slashes=False)
-def c(text):
-    return 'C {}'.format(text.replace("_", " "))
-
-
-
 @app.route("/python", strict_slashes=False)
 @app.route("/python/<text>", strict_slashes=False)
 def python(text="is cool"):
