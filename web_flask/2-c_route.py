aba8415cb480e60b4b5ccd7b05f86552833c94b7
islam solaiman <islam.solaiman92@gmail.com>
2023-10-21 09:55:56 -0700

    web_flask - Task 2

---
+++ web_flask/2-c_route.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+    script that starts a Flask web application
+    web application must be listening on 0.0.0.0, port 5000
+    Routes: /: display “Hello HBNB!”
+    /hbnb: display “HBNB”
+    /c/<text>: display “C ” followed by the value of the text variable
+    (replace underscore _ symbols with a space )
+"""
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    return "HBNB"
+
+
+def c(text):
+    return 'c {}'.format(text.replace('_', ' '))
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port='5000')
