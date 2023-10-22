66717fd3d45d55b25a2af4c0a184c2ef333a882a
islam solaiman <islam.solaiman92@gmail.com>
2023-10-22 15:36:37 -0700

    AirBnB clone - Web framework

---
+++ web_flask/10-hbnb_filters.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+     script that starts a Flask web application
+     application must be listening on 0.0.0.0, port 5000
+"""
+
+from flask import Flask
+from models import storage
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route("/hbnb_filters", strict_slashes=False)
+def hbnb_filters():
+    states = storage.all("State")
+    amenities = storage.all("Amenity")
+    return render_template("10-hbnb_filters.html",
+                           states=states, amenities=amenities)
+
+
+@app.teardown_appcontext
+def teardown(exc):
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
