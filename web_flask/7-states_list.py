5548d50895ecc45e0b2ccb9725cff98e9b1ecb17
islam solaiman <islam.solaiman92@gmail.com>
2023-10-22 14:47:27 -0700

    Web framework - Task 7

---
+++ web_flask/7-states_list.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+     script that starts a Flask web application
+     application must be listening on 0.0.0.0, port 5000
+"""
+
+from flask import Flask
+from models import storage
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route("/states_list", strict_slashes=False)
+def states_list():
+    states = storage.all("State")
+    return render_template("7-states_list.html", states=states)
+
+
+@app.teardown_appcontext
+def teardown(exc):
+    storage.close()
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
